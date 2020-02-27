1b97c22ce01359df669ec191e9089a6b028439ff
iperezx <i3perez@sdsc.edu>
2020-02-26 18:23:36 -0800

    Add download (minio sdk).

--- routes.go
+++ routes.go
@@ -2,27 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/minio/minio-go/v6"
 )
 
 func listByBucket(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-
-	endpoint := ""
-	accessKeyID := ""
-	secretAccessKey := ""
-	useSSL := true
-	// Initialize minio client object.
-	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Printf("%#v\n", minioClient) // minioClient is now setup
-
 	buckets, err := minioClient.ListBuckets()
 	if err != nil {
 		fmt.Println(err)
@@ -31,4 +20,35 @@ func listByBucket(w http.ResponseWriter, r *http.Request) {
 	for _, bucket := range buckets {
 		fmt.Fprintf(w, "Bucket: %v\n", bucket)
 	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func getObjectFromBucket(w http.ResponseWriter, r *http.Request) {
+	//Parse
+	pathParams := mux.Vars(r)
+	bucketName := pathParams["bucket"]
+	objectName := pathParams["object"]
+	//Check if Bucket exist
+	exists, errBucketExists := minioClient.BucketExists(bucketName)
+	if !exists && errBucketExists != nil {
+		log.Printf("Bucket: %s does not exist.", bucketName)
+		log.Fatalln(errBucketExists)
+	}
+	object, err := minioClient.GetObject(bucketName, objectName, minio.GetObjectOptions{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	localFile, err := os.Create(filePath + objectName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if _, err = io.Copy(localFile, object); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprintf(w, "Download - Bucket: %v and Object: %v\n", bucketName, objectName)
+	fmt.Fprintf(w, "Destination: %v\n", filePath+objectName)
+	w.WriteHeader(http.StatusOK)
 }
--- server.go
+++ server.go
@@ -4,17 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/minio/minio-go/v6"
 )
 
+var (
+	endpoint        string
+	accessKeyID     string
+	secretAccessKey string
+	useSSL          bool
+	minioClient     *minio.Client
+	err             error
+	filePath        string
+)
+
+func init() {
+	endpoint = os.Args[1]
+	accessKeyID = os.Args[2]
+	secretAccessKey = os.Args[3]
+	filePath = os.Args[4]
+	useSSL = true
+	// Initialize minio client object.
+	minioClient, err = minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("%#v\n", minioClient)
+}
+
 func main() {
+	fmt.Println(os.Args[1:])
 	r := mux.NewRouter()
-	log.Println("object storage api")
+	log.Println("Sage REST API")
 	api := r.PathPrefix("/api/v1").Subrouter()
 	api.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "api v1")
 	})
 	api.HandleFunc("/buckets", listByBucket).Methods(http.MethodGet)
+	api.HandleFunc("/buckets/{bucket}/{object}", getObjectFromBucket).Methods(http.MethodGet)
 	log.Fatalln(http.ListenAndServe(":8080", r))
 }

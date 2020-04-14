668cf52705907e89286a6a44af49df6194ba50c7
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2020-04-14 10:30:49 -0500

    added uploadObject and downloadObject routes

--- routes.go
+++ routes.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 
@@ -95,41 +96,103 @@ func putObjectInBucket(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "Upload - Bucket: %v and Object: %v\n", bucketName, objectName)
 	w.WriteHeader(http.StatusOK)
+
+}
+
+// Data simple response object
+type Data struct {
+	ID       string             `json:"bucket-id,omitempty"`
+	Key      string             `json:"key,omitempty"`
+	Metadata map[string]*string `json:"metadata,omitempty"`
+	Error    string             `json:"error,omitempty"`
 }
 
 func uploadObject(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(maxMemory)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "body not parsed"}`))
+
+	vars := mux.Vars(r)
+
+	username := vars["username"]
+
+	log.Printf("username: %s", username)
+
+	data := Data{}
+
+	query := r.URL.Query()
+	dataTypeArray, ok := query["type"]
+
+	if !ok {
+		respondJSONError(w, http.StatusInternalServerError, "Please specify data type via query field \"type\"")
 		return
 	}
 
-	query := r.URL.Query()
-	dataType := query.Get("type")
+	if len(dataTypeArray) == 0 {
+		respondJSONError(w, http.StatusInternalServerError, "Please specify data type via query field \"type\"")
+		return
+	}
+
+	dataType := dataTypeArray[0]
+
 	if dataType == "" {
-		http.Error(w, "Please specify data type via query field \"type\"", http.StatusInternalServerError)
+		respondJSONError(w, http.StatusInternalServerError, "Please specify data type via query field \"type\"")
 		return
 	}
 	dataType = strings.ToLower(dataType)
-	_, ok := validDataTypes[dataType]
+	_, ok = validDataTypes[dataType]
 	if !ok {
-		http.Error(w, fmt.Sprintf("Data type %s not supported", dataType), http.StatusInternalServerError)
+		respondJSONError(w, http.StatusInternalServerError, "Data type %s not supported", dataType)
+
 		return
 
+	}
+
+	err := r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondJSONError(w, http.StatusBadRequest, "ParseMultipartForm returned: %s", err.Error())
+		return
 	}
 
 	newUUID, err := uuid.NewRandom()
 	if err != nil {
-		http.Error(w, "error generateing uuid", http.StatusInternalServerError)
+		respondJSONError(w, http.StatusInternalServerError, "error generateing uuid %s", err.Error())
 		return
 
 	}
 
 	newUUIDStr := newUUID.String()
+
+	data.ID = newUUIDStr
 	objectName := newUUIDStr
 
-	bucketName := dataType
+	bucketName := "sagedata-" + newUUIDStr[0:2] // first two characters of uuid
+	log.Printf("bucketName: %s", bucketName)
+
+	_, err = svc.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(bucketName),
+	})
+	if err != nil {
+		log.Printf("bucket creation error: %s ", err.Error())
+		// skip creation if it already exists
+		if !strings.HasPrefix(err.Error(), s3.ErrCodeBucketAlreadyOwnedByYou) {
+
+			fmt.Printf("Waiting for bucket %q to be created...\n", bucketName)
+
+			err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
+				Bucket: aws.String(bucketName),
+			})
+
+			if err != nil {
+				respondJSONError(w, http.StatusInternalServerError, "Unable to create bucket %q, %v", bucketName, err)
+				return
+			}
+
+			log.Printf("bucket %s created", bucketName)
+		}
+
+	}
+
+	// Wait until bucket is created before finishing
+
+	//bucketName := dataType
 	file, header, err := r.FormFile("file")
 	//objectName := header.Filename
 	if err != nil {
@@ -141,14 +204,24 @@ func uploadObject(w http.ResponseWriter, r *http.Request) {
 	var objectMetadata map[string]*string
 	objectMetadata = make(map[string]*string)
 
+	data.Metadata = objectMetadata
 	filename := header.Filename
+
 	log.Printf("filename: %s", filename)
-	if filename != "" {
-		objectMetadata["filename"] = &filename
+	if filename == "" {
+		respondJSONError(w, http.StatusInternalServerError, "Filename missing")
+		return
 	}
+	key := filename // use filename unless key has been specified
+
+	objectMetadata["owner"] = &username
+	objectMetadata["type"] = &dataType
+
+	data.Key = key
+
 	upParams := &s3manager.UploadInput{
 		Bucket:   aws.String(bucketName),
-		Key:      aws.String(objectName),
+		Key:      aws.String(objectName + "/" + filename),
 		Body:     file,
 		Metadata: objectMetadata,
 	}
@@ -160,12 +233,88 @@ func uploadObject(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Printf("Upload - Bucket: %v and Object: %v\n", bucketName, objectName)
+	//log.Printf("Upload - Bucket: %v and Object: %v\n", bucketName, objectName)
+
+	respondJSON(w, http.StatusOK, data)
+}
+
+func downloadObject(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	uuidStr := pathParams["bucket"]
+	sageKey := pathParams["key"]
+
+	// convert SAGE specifiers to S3 specifiers
+	bucketName := "sagedata-" + uuidStr[0:2]
+	key := path.Join(uuidStr, sageKey)
+
+	log.Printf("bucketName: %s key: %s", bucketName, key)
+
+	out, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: &bucketName,
+		Key:    &key,
+	})
+	if err != nil {
+		respondJSONError(w, http.StatusInternalServerError, "Error getting data, svc.GetObject returned: %s", err.Error())
+		return
+	}
+	defer out.Body.Close()
+
+	w.Header().Set("Content-Disposition", "attachment; filename="+sageKey)
+	//w.Header().Set("Content-Length", FileSize)
+
+	buffer := make([]byte, 1024*1024)
+	for {
+		n, err := out.Body.Read(buffer)
+		if err != nil {
+
+			if err == io.EOF {
+				w.Write(buffer[:n]) //should handle any remainding bytes.
+				break
+			}
+
+			respondJSONError(w, http.StatusInternalServerError, "Error getting data: %s", err.Error())
+			return
+		}
+		w.Write(buffer)
+		//fmt.Println(string(p[:n]))
+	}
+
 	w.WriteHeader(http.StatusOK)
 }
 
-func mw(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	tokenStr := r.FormValue("token")
+func respondJSON(w http.ResponseWriter, statusCode int, data Data) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
+}
+
+func respondJSONError(w http.ResponseWriter, statusCode int, msg string, args ...interface{}) {
+	errorStr := fmt.Sprintf(msg, args...)
+	respondJSON(w, statusCode, Data{Error: errorStr})
+}
+
+func authMW(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+
+	authorization := r.Header.Get("Authorization")
+	if authorization == "" {
+		respondJSONError(w, http.StatusInternalServerError, "Authorization header is missing")
+		return
+	}
+	log.Printf("authorization: %s", authorization)
+	authorizationArray := strings.Split(authorization, " ")
+	if len(authorizationArray) != 2 {
+		respondJSONError(w, http.StatusInternalServerError, "Authorization field must be of form \"sage <token>\"")
+		return
+	}
+
+	if strings.ToLower(authorizationArray[0]) != "sage" {
+		respondJSONError(w, http.StatusInternalServerError, "Only bearer \"sage\" supported")
+		return
+	}
+
+	//tokenStr := r.FormValue("token")
+	tokenStr := authorizationArray[1]
+	log.Printf("tokenStr: %s", tokenStr)
 	url := tokenInfoEndpoint
 
 	log.Printf("url: %s", url)
@@ -235,6 +384,22 @@ func mw(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		http.Error(w, "token not active", http.StatusInternalServerError)
 		return
 	}
+
+	usernameIf, ok := dat["username"]
+	if !ok {
+		respondJSONError(w, http.StatusInternalServerError, "username is missing")
+		return
+	}
+
+	username, ok := usernameIf.(string)
+	if !ok {
+		respondJSONError(w, http.StatusInternalServerError, "username is not string")
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	vars["username"] = username
 
 	next(w, r)
 

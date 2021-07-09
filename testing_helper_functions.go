75ccc5332f7aeb30ef4bb0a82230923d69302b17
Marie Yau <myau@lbl.gov>
2021-07-08 18:38:48 -0700

    added helper function for geting all bucket IDs

--- testing_helper_functions.go
+++ testing_helper_functions.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 )
 
 func deleteSingleBucket(bucketID string, username string) {
@@ -20,11 +22,30 @@ func deleteSingleBucket(bucketID string, username string) {
 	rr := httptest.NewRecorder()
 
 	mainRouter.ServeHTTP(rr, req)
-	log.Printf(bucketID + " deleted")
+	log.Printf("Deleted: " + bucketID)
 }
 
 func deleteMultipleBuckets(bucketIDs []string, username string) {
 	for _, bucketID := range bucketIDs {
 		deleteSingleBucket(bucketID, username)
 	}
+}
+
+func getAllBucketIDs(username string) (bucketIDs []string) {
+	rr := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "/api/v1/objects", nil)
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+	req.Header.Add("Authorization", "sage user:"+username)
+	mainRouter.ServeHTTP(rr, req)
+
+	var buckets []SAGEBucket
+	_ = json.Unmarshal([]byte(rr.Body.String()), &buckets)
+
+	for _, bucket := range buckets {
+		bucketIDs = append(bucketIDs, bucket.ID)
+	}
+	log.Printf("Bucket IDs: " + strings.Join(bucketIDs, ", "))
+	return
 }
